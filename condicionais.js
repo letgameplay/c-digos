2c3bf5e0621f00c016b67cd8fa4913e5c93e73cc
Letgameplays <leticiabarbosa9678@gmail.com>
2024-03-08 08:31:19 -0300

    atualiza

---
+++ condicionais.js
@@ -0,0 +1,13 @@
+/*
+python
+idade = 18
+if idade >= 18:
+print ("Você é maior de idade.)
+*/
+
+let idade = 17;
+if (idade >= 18) {
+    console.log ("Você é maior de idade.") // é utilizado as chaves em vez dos dois pontos
+    console.log ("Já pode ser preso.")
+}
+console.log("Terminou!")
---
+++ dados.js
@@ -0,0 +1,14 @@
+
+       console.log(typeof(msg1));
+console.log(typeof(idade));
+
+
+console.log (5 == 5); //compara conteúdo
+console.log (5 === 5); //compara conteúdo e tipo
+console.log (5 == '5');
+console.log (5 === '5');
+
+console.log(true && false); //AND, verdadeiro só se ambos forem verdadeiros
+console.log( 1 && 1);
+console.log(1 || 0); //OR, basta um ser vedadeiro
+
