c525102b3fa8984c7fb2518435a64c4a1b02f534
lelet <leticiabarbosa9678@gmail.com>
2024-03-15 08:28:31 -0300

    quinze

---
+++ declarandofuncoes.js
@@ -0,0 +1,5 @@
+function quadrado (n) {
+    return n ** 2;
+}
+
+console.log(quadrado(8));
--- fatorial.js
+++ fatorial.js
@@ -1,3 +1,4 @@
+/*
 function fatorial(n) {
     let fator = 1
     for (let i = 2; i <= n; i ++)
@@ -5,3 +6,13 @@ function fatorial(n) {
     return fator
 }
 console.log(fatorial(5));
+*/
+
+function fatorial() {
+    let n = document.getElementById("numero").value;
+    let fator = 1
+    for (let i = 2; i <= n; i ++)
+        fator = i * fator
+    document.getElementById("resultado").innerHTML =
+    `Fatorial(${n}) = ${fator}`;
+}
