46c955b7119182025c8e3adaca0970377ddefa4d
Letgameplays <leticiabarbosa9678@gmail.com>
2024-03-22 08:28:50 -0300

    novas

---
+++ listadeexercicio1/questao20.js
@@ -0,0 +1,6 @@
+function cadast() {
+    email = document.getElementById('em').value 
+    email = document.getElementById('em')
+    email = document.getElementById('em')
+    email = document.getElementById('em')
+}
