79da88dc4d081d7f3024f828c90e98624aa3d64c
Your Name <leticiabarbosa9678@gmail.com>
2024-07-05 08:27:56 -0300

    vcvbbf

---
+++ nomes.js
@@ -0,0 +1,7 @@
+function nb() {
+    let nome = document.getElementById("n").value;
+    let sobrenome = document.getElementById("s").value;
+
+    document.getElementById("resultado").innerHTML =
+    `${nome} ${sobrenome}`
+}
