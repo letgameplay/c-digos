4dd294d2f2f15e6219199e774c93a9aa2161b31a
Your Name <leticiabarbosa9678@gmail.com>
2024-07-12 08:16:21 -0300

    hghghgvghg

--- IFRN/listadeexercicio1/10 ate 20/questao20.js
+++ IFRN/listadeexercicio1/10 ate 20/questao20.js
@@ -1,6 +1,31 @@
 function cadast() {
-    email = document.getElementById('em').value 
-    email = document.getElementById('em')
-    email = document.getElementById('em')
-    email = document.getElementById('em')
-}
+    nome = document.getElementById('nm').value.trim();
+    email = document.getElementById('em').value.trim();
+    telefone = document.getElementById('tele').value.trim();
+    senha = document.getElementById('sen').value                                                                                                                                                                         
+    confirmacao = document.getElementById('confse').value
+
+    ernome = alert("nomeerror")
+    eremail = alert("emailerror")
+    ertel = alert("telerror")
+    ersenha = alert("senhaerror")
+    erconf = alert("conferror")
+
+    valido = true
+
+    if (nome === ""){
+        ernome.textContent = "NOME NÃO INFORMADO";
+        valido = false }
+        else {
+            nomeerror.textContent = ""
+        }
+
+        if (email === "") {
+            ermail.textContent = "EMAIL NÃO INFORMADO";
+            valido = false; }
+            else if (!validarEmail(email)) {
+                ermail.textContent = "EMAIL INVÁLIDO"
+                valido = false            }
+        }
+
+
