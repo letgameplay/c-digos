07aa09a25f0b6f9414e98377eb0020b832c5703e
Your Name <leticiabarbosa9678@gmail.com>
2024-07-15 08:09:19 -0300

    jhjjhh

--- IFRN/listadeexercicio1/10 ate 20/questao20.js
+++ IFRN/listadeexercicio1/10 ate 20/questao20.js
@@ -1,31 +1,27 @@
 function cadast() {
-    nome = document.getElementById('nm').value.trim();
-    email = document.getElementById('em').value.trim();
-    telefone = document.getElementById('tele').value.trim();
-    senha = document.getElementById('sen').value                                                                                                                                                                         
-    confirmacao = document.getElementById('confse').value
+    let nome = document.getElementById('nm').value.trim();
+    let email = document.getElementById('em').value.trim();
+    let telefone = document.getElementById('tele').value.trim();
+    let senha = document.getElementById('sen').value.trim()                                                                                                                                                                        
+    let confirmacao = document.getElementById('confse').value.trim()
 
-    ernome = alert("nomeerror")
-    eremail = alert("emailerror")
-    ertel = alert("telerror")
-    ersenha = alert("senhaerror")
-    erconf = alert("conferror")
+    let regex = /^(?=.*[a-zA-Z\d]).{8,}$/;
 
-    valido = true
-
-    if (nome === ""){
-        ernome.textContent = "NOME NÃO INFORMADO";
-        valido = false }
-        else {
-            nomeerror.textContent = ""
-        }
-
-        if (email === "") {
-            ermail.textContent = "EMAIL NÃO INFORMADO";
-            valido = false; }
-            else if (!validarEmail(email)) {
-                ermail.textContent = "EMAIL INVÁLIDO"
-                valido = false            }
-        }
-
-
+    if (!nome) {
+        alert('PREENCHA O NOME') }
+    else if (!email) {
+        alert('PREENCHA O EMAIL') }
+    else if (!telefone) {
+        alert('PREENCHA O TELEFONE') }
+    else if (!senha) {
+        alert('PREENCHA O NOME') }
+    else if (!confirmacao) {
+        alert("CONFIRME A SENHA") }
+    else if (senha != confirmacao) {
+        alert("SENHA DIVERGENTE") }
+    else if (!email.includes('@') || !email.includes('.')) {
+        alert("EMAIL INVALIDO") }
+    else if (!regex.test(senha)) {
+        alert("SENHA INVALIDA") }
+    else {
+        alert("CADASTRO REALIZADO COM SUCESSO"); } }
