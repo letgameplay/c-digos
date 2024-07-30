6b602122018412b33d44385ac2a8615f363dd3c4
leletgameplays <leticiabarbosa9678@gmail.com>
2024-07-29 21:51:08 -0300

    oii

--- IFRN/listadeexercicio1/21 ate 27/jogodavelha.js
+++ IFRN/listadeexercicio1/21 ate 27/jogodavelha.js
@@ -1,1 +1,62 @@
-/* this - se refere a sua celula, substitui o id */
+let jogador = 'X';
+let jogo = [['', '', ''],
+            ['', '', ''],
+            ['', '', '']]
+let gameover = false; // quando gameover é true o jogo deve parar
+
+
+function marcar(celula, linha, coluna) {
+    if (gameover || jogo[linha][coluna]) return; // or, apenas um precisa ser verdadeiro
+    // Se gameover for true, o jogo termina. Se a célula especificada
+    // não estiver vazia a função retorna sem fazer alterações
+
+
+    celula.textContent = jogador; // mostra a marca do jogador na celula
+    jogo[linha][coluna] = jogador; // atualiza o estado do jogo interno
+
+
+    if (checarvitoria(jogador)) {
+        document.getElementById('resultado').textContent = 'Jogador ' + jogador + ' venceu!';
+        gameover = true;
+        return;
+    }
+    jogador = (jogador === 'X') ? 'O' : 'X';
+    // CONDIÇÃO | VALOR SE VERDADEIRO : VALOR SE FALSO    
+}
+
+
+//primeiro linha segundo coluna
+function checarvitoria(jog){
+    for (var i = 0; i < 3; i++) {
+        if (jogo[i][0] === jog && jogo[i][1] === jog && jogo [i][2] === jog){
+        return true;
+
+        }
+
+        // verifica as linhas da matriz, então i percorre de 0 a 2, começando pela linha 0
+        // se o indice da linha 0 na coluna 0 for igual a jog (valor do jogador)
+        // se os tres forem igual a jog, a vitória é true
+
+        if (jogo[0][i] === jog &&  jogo[1][i] === jog && jogo [2][i] === jog){
+        return true;
+        }
+    
+    }
+
+        if (jogo[0][0] === jog && jogo[1][1] === jog && jogo[2][2] === jog){
+        return true;
+    
+    }
+
+        if (jogo[0][2] === jog && jogo[1][1] === jog && jogo[2][0] === jog){
+        return true;
+    
+    }
+       
+        return false;
+
+}
+   
+
+
+// 3x3 3 linhas e 3 colunas
