66da43bc1ae176b146d4521806bde4914ace132a
leletgameplays <leticiabarbosa9678@gmail.com>
2024-09-22 22:56:47 -0300

    peoooooo

--- nomes.js
+++
@@ -1,7 +0,0 @@
-function nb() {
-    let nome = document.getElementById("n").value;
-    let sobrenome = document.getElementById("s").value;
-
-    document.getElementById("resultado").innerHTML =
-    `${nome} ${sobrenome}`
-}
